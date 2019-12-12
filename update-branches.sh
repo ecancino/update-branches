ffdd023b40c9565ee1f8a5a64765bf1c7bab8cf3
Eduardo Cancino <ezarate@liaisonedu.com>
2019-12-12 12:20:58 -0500

    Add usage information; Add parameter from upstream branch Added condition to update the branch and to push only if the remote exists

--- update-branches.sh
+++ update-branches.sh
@@ -2,32 +2,58 @@
 
 source "$(dirname -- ${0})/status.sh";
 
-echo "Entering repo"
-cd $1;
-if [ $? -eq 0 ];
-then 
-    echo "Fetching..."
-    git checkout develop --quiet;
-    git fetch --quiet;
+bold() {
+    printf "$1${BWhite}$2${Reset}$3"
+}
+
+em() {
+    printf "$1${Yellow}$2${Reset}$3"
+}
+
+display_usage() { 
+    em "" "USAGE: \n" 
+    bold "" "update_branches " "repository_path [upstream_branch]\n"
+    bold "  " "repository_path"   "   Specify the repository folder to update\n"
+    bold "  " "upstream_branch"   "   Specify the branch to update from\n"
+    exit 1
+} 
+
+REPO_PATH=$1
+UPSTREAM_BRANCH=${2:-"develop"}
+SKIP_BRANCHES=("develop", "master")
+
+if [  $# -eq 0 ]; then 
+    display_usage
+    exit 1
+fi 
+
+em "Entering " $REPO_PATH
+cd $REPO_PATH;
+st "$?"
+
+if [ $? -eq 0 ]; then 
+    em "Update " $UPSTREAM_BRANCH
+    git checkout "$UPSTREAM_BRANCH" --quiet;
+    git pull --rebase origin "$UPSTREAM_BRANCH"  --quiet;
     st "$?"
 
-    echo "Retrieving branches..."
     for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/); do
-        if [ "$branch" != "develop" ] 
+        # if [ "$branch" != "develop" ] && [ "$branch" != "master" ]
+        if [[ ! "${SKIP_BRANCHES[@]}" =~ "${branch}" ]];
         then
+            # printf "Updating ${Yellow}$branch${Reset}\n"
+            em "Updating " $branch "\n"
+
+            printf "\t⏱: Cheking out";
+            git checkout "$branch" --quiet;
+            st "$?";
+
+            em "\t⏱: Rebase from " $UPSTREAM_BRANCH
+            git rebase "$UPSTREAM_BRANCH" --quiet;
+            st "$?";
+
             git ls-remote --exit-code --quiet --heads origin "$branch" > /dev/null;
-            if [ $? -eq 0 ]; 
-            then    
-                printf "Updating ${On_Green}${Yellow}$branch${Reset}\n"
-
-                printf "\t⏱: Cheking out";
-                git checkout "$branch" --quiet;
-                st "$?";
-
-                printf "\t⏱: Rebase develop";
-                git rebase develop --quiet;
-                st "$?";
-
+            if [ $? -eq 0 ]; then    
                 printf "\t⏱: Update remote ";
                 git push --force-with-lease --quiet;
                 st "$?";
@@ -35,7 +61,9 @@ then
         fi
     done
 
-    git checkout develop --quiet;
+    echo " "
+
+    git checkout "$UPSTREAM_BRANCH" --quiet;
 
     printf "${Green}DONE${Reset}!\n"; 
 fi
